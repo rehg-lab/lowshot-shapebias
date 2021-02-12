8fd596f04f20557febd8b2151cf92f1cf9691077
Stefan Stojanov <sstojanov@users.noreply.github.com>
2021-02-12 14:47:22 -0500

    minor update

--- download_data.sh
+++ download_data.sh
@@ -7,17 +7,17 @@ cd data
 wget https://dl.dropbox.com/s/7lojtne1v6kgawe/ModelNet40-LS.tar
 
 # download shapenet
-#wget https://dl.dropbox.com/s/4cp2cn0vx3ltw7p/ShapeNet55-LS.tar
+wget https://dl.dropbox.com/s/4cp2cn0vx3ltw7p/ShapeNet55-LS.tar
 
 # download toys
-#wget https://dl.dropbox.com/s/qjlc1smbg599xi7/TOYS4K.tar
+wget https://dl.dropbox.com/s/qjlc1smbg599xi7/TOYS4K.tar
 
 # untar data
 tar -xf ModelNet40-LS.tar
-#tar -xf ShapeNet55-LS.tar
-#tar -xf TOYS4K.tar
+tar -xf ShapeNet55-LS.tar
+tar -xf TOYS4K.tar
 
 # remove tarballs
 rm ModelNet40-LS.tar
-#rm ShapeNet55-LS.tar
-#rm TOYS4K.tar
+rm ShapeNet55-LS.tar
+rm TOYS4K.tar
